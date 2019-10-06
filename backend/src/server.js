146b0bfc229e824ea3f880208cec9e98819b5601
lucasmori <lucastecnew@gmail.com>
2019-10-06 18:20:17 -0300

    Alterações para funcionar com o frontend

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const routes = require('./routes');
+const cors = require('cors');
+const path = require('path');
 
 const app = express();
-
 
 mongoose.connect('mongodb+srv://omnistack:omnistack@omnistack-ewxik.mongodb.net/aircnc?retryWrites=true&w=majority', {
   useNewUrlParser: true,
@@ -16,7 +17,9 @@ mongoose.connect('mongodb+srv://omnistack:omnistack@omnistack-ewxik.mongodb.net/
 // req.params = Acessar route params (para edicao, delete)
 // req.body = Acessar corpo da requisicao (para criacao, edicao)
 
+app.use(cors());
 app.use(express.json());
+app.use('/files', express.static(path.resolve(__dirname, '..', 'uploads')));
 app.use(routes);
 
 app.listen(3333);
